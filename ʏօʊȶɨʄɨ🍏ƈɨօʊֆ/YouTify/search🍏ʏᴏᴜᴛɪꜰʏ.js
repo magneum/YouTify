580aa05cff539f26e12ea97d506f8168f6b577cf
krakinz <krakinzkon@gmail.com>
2021-10-07 18:17:01 +0100

    🍏YouTify™ - v5 - ʏօʊȶɨʄɨ🍏ƈɨօʊֆ

--- ʏօʊȶɨʄɨ🍏ƈɨօʊֆ/YouTify/search🍏ʏᴏᴜᴛɪꜰʏ.js
+++ ʏօʊȶɨʄɨ🍏ƈɨօʊֆ/YouTify/search🍏ʏᴏᴜᴛɪꜰʏ.js
@@ -1,6 +1,6 @@
 const { _Youtify_, YouTify_Song_Feeder } = require("../🍏/_Youtify_.js"),
   ʏᴏᴜᴛɪꜰʏʏᴛꜱʀ = require("youtube-sr").default,
-  ʏᴏᴜᴛɪꜰʏʏᴛᴅʟ = require("@distube/ytdl");
+  ʏᴏᴜᴛɪꜰʏʏᴛᴅʟ = require("../ɖɨֆȶʊɮɛ/ʏȶɖʟ");
 const { MessageEmbed } = require("../ӄʀǟӄɨռʐʟǟɮ");
 const ᴅɪꜱᴄᴏʀᴅ = require(`../ӄʀǟӄɨռʐʟǟɮ`);
 module.exports.run = async (client, message, args) => {
