963779f2d6cdc7d1eb6d58581baa9f351529c6e4
HypeVoidSoul <hypevoidsoul@gmail.com>
2021-10-10 06:46:35 +0100

    🍏YouTify™ v8 Huge Rework 🍏 + New Filters

--- YouTify_Genres/Filters/bassboost.js
+++ YouTify_Genres/Filters/bassboost.js
@@ -1,5 +1,8 @@
 const { YouTify_Singer } = require("../../YouTifyPlayer/YouTify_Singer.js");
-
+const { MessageEmbed } = require("discord.js");
+// ===========================================================================================================================
+// 🍏𝐘𝐨𝐮𝐓𝐢𝐟𝐲™ is Discord 𝐘𝐎𝐔𝐓𝐔𝐁𝐄 Music Bot built with Discord.js and has 𝟐𝟎+ 𝐀𝐮𝐝𝐢𝐨 𝐅𝐢𝐥𝐭𝐞𝐫𝐬. ❓𝘚𝘱𝘰𝘵𝘪𝘧𝘺 𝘢𝘯𝘥 𝘚𝘰𝘶𝘯𝘥𝘤𝘭𝘰𝘶𝘥 𝘢𝘳𝘦 𝘪𝘯 𝘣𝘦𝘵𝘢❓
+// ===========================================================================================================================
 module.exports.run = async (client, message, args, Discord) => {
   const Channel = message.member.voice.channel;
   if (!Channel) return message.channel.send("Error: No Voice Channel!");
@@ -9,29 +12,32 @@ module.exports.run = async (client, message, args, Discord) => {
     return message.channel.send(
       "Error: No Queue, Please Add Some Songs By Using Play & Search Command!"
     );
-
-  const Embed = new Discord.MessageEmbed()
-    .setColor(client.Color)
-    .setAuthor("Bassboost", message.author.avatarURL({ dynamic: true }))
-    .setDescription(
-      `Bassboost Filter Has Been ${
-        Queue.Filters["bassboost"]
-          ? "Disabled (Song Maybe Backward)"
-          : "Enabled (Song Maybe Ahead)"
-      }!`
-    )
-    .setTimestamp();
-
+  // ===========================================================================================================================
+  // 🍏𝐘𝐨𝐮𝐓𝐢𝐟𝐲™ is Discord 𝐘𝐎𝐔𝐓𝐔𝐁𝐄 Music Bot built with Discord.js and has 𝟐𝟎+ 𝐀𝐮𝐝𝐢𝐨 𝐅𝐢𝐥𝐭𝐞𝐫𝐬. ❓𝘚𝘱𝘰𝘵𝘪𝘧𝘺 𝘢𝘯𝘥 𝘚𝘰𝘶𝘯𝘥𝘤𝘭𝘰𝘶𝘥 𝘢𝘳𝘦 𝘪𝘯 𝘣𝘦𝘵𝘢❓
+  // ===========================================================================================================================
   Queue.Filters["bassboost"] = !Queue.Filters["bassboost"];
-
   await YouTify_Singer(client, message, {
     Filter: true,
     Song: Queue.Songs[0],
   });
-
-  return message.channel.send(Embed);
+  message.channel.send(
+    new MessageEmbed()
+      .setColor(client.Color)
+      .setAuthor("Bassboost", message.author.avatarURL({ dynamic: true }))
+      .setDescription(
+        `Bassboost Filter Has Been ${
+          Queue.Filters["bassboost"]
+            ? "Disabled (Song Maybe Backward)"
+            : "Enabled (Song Maybe Ahead)"
+        }!`
+      )
+      .setTimestamp()
+  );
+  return;
 };
-
+// ===========================================================================================================================
+// 🍏𝐘𝐨𝐮𝐓𝐢𝐟𝐲™ is Discord 𝐘𝐎𝐔𝐓𝐔𝐁𝐄 Music Bot built with Discord.js and has 𝟐𝟎+ 𝐀𝐮𝐝𝐢𝐨 𝐅𝐢𝐥𝐭𝐞𝐫𝐬. ❓𝘚𝘱𝘰𝘵𝘪𝘧𝘺 𝘢𝘯𝘥 𝘚𝘰𝘶𝘯𝘥𝘤𝘭𝘰𝘶𝘥 𝘢𝘳𝘦 𝘪𝘯 𝘣𝘦𝘵𝘢❓
+// ===========================================================================================================================
 module.exports.help = {
   name: "bassboost",
   aliases: ["bb"],
--- YouTify_Genres/Filters/nightcore.js
+++ YouTify_Genres/Filters/nightcore.js
@@ -1,37 +1,45 @@
 const { YouTify_Singer } = require("../../YouTifyPlayer/YouTify_Singer.js");
-
+const { MessageEmbed } = require("discord.js");
+// ===========================================================================================================================
+// 🍏𝐘𝐨𝐮𝐓𝐢𝐟𝐲™ is Discord 𝐘𝐎𝐔𝐓𝐔𝐁𝐄 Music Bot built with Discord.js and has 𝟐𝟎+ 𝐀𝐮𝐝𝐢𝐨 𝐅𝐢𝐥𝐭𝐞𝐫𝐬. ❓𝘚𝘱𝘰𝘵𝘪𝘧𝘺 𝘢𝘯𝘥 𝘚𝘰𝘶𝘯𝘥𝘤𝘭𝘰𝘶𝘥 𝘢𝘳𝘦 𝘪𝘯 𝘣𝘦𝘵𝘢❓
+// ===========================================================================================================================
 module.exports.run = async (client, message, args, Discord) => {
   const Channel = message.member.voice.channel;
   if (!Channel) return message.channel.send("Error: No Voice Channel!");
-
+  // ===========================================================================================================================
+  // 🍏𝐘𝐨𝐮𝐓𝐢𝐟𝐲™ is Discord 𝐘𝐎𝐔𝐓𝐔𝐁𝐄 Music Bot built with Discord.js and has 𝟐𝟎+ 𝐀𝐮𝐝𝐢𝐨 𝐅𝐢𝐥𝐭𝐞𝐫𝐬. ❓𝘚𝘱𝘰𝘵𝘪𝘧𝘺 𝘢𝘯𝘥 𝘚𝘰𝘶𝘯𝘥𝘤𝘭𝘰𝘶𝘥 𝘢𝘳𝘦 𝘪𝘯 𝘣𝘦𝘵𝘢❓
+  // ===========================================================================================================================
   const Queue = client.queue.get(message.guild.id);
   if (!Queue)
     return message.channel.send(
       "Error: No Queue, Please Add Some Songs By Using Play & Search Command!"
     );
-
-  const Embed = new Discord.MessageEmbed()
-    .setColor(client.Color)
-    .setAuthor("Nightcore", message.author.avatarURL({ dynamic: true }))
-    .setDescription(
-      `Nightcore Filter Has Been ${
-        Queue.Filters["nightcore"]
-          ? "Disabled (Song Maybe Backward)"
-          : "Enabled (Song Maybe Ahead)"
-      }!`
-    )
-    .setTimestamp();
-
+  // ===========================================================================================================================
+  // 🍏𝐘𝐨𝐮𝐓𝐢𝐟𝐲™ is Discord 𝐘𝐎𝐔𝐓𝐔𝐁𝐄 Music Bot built with Discord.js and has 𝟐𝟎+ 𝐀𝐮𝐝𝐢𝐨 𝐅𝐢𝐥𝐭𝐞𝐫𝐬. ❓𝘚𝘱𝘰𝘵𝘪𝘧𝘺 𝘢𝘯𝘥 𝘚𝘰𝘶𝘯𝘥𝘤𝘭𝘰𝘶𝘥 𝘢𝘳𝘦 𝘪𝘯 𝘣𝘦𝘵𝘢❓
+  // ===========================================================================================================================
   Queue.Filters["nightcore"] = !Queue.Filters["nightcore"];
-
   await YouTify_Singer(client, message, {
     Filter: true,
     Song: Queue.Songs[0],
   });
-
-  return message.channel.send(Embed);
+  message.channel.send(
+    new MessageEmbed()
+      .setColor(client.Color)
+      .setAuthor("Nightcore", message.author.avatarURL({ dynamic: true }))
+      .setDescription(
+        `Nightcore Filter Has Been ${
+          Queue.Filters["nightcore"]
+            ? "Disabled (Song Maybe Backward)"
+            : "Enabled (Song Maybe Ahead)"
+        }!`
+      )
+      .setTimestamp()
+  );
+  return;
 };
-
+// ===========================================================================================================================
+// 🍏𝐘𝐨𝐮𝐓𝐢𝐟𝐲™ is Discord 𝐘𝐎𝐔𝐓𝐔𝐁𝐄 Music Bot built with Discord.js and has 𝟐𝟎+ 𝐀𝐮𝐝𝐢𝐨 𝐅𝐢𝐥𝐭𝐞𝐫𝐬. ❓𝘚𝘱𝘰𝘵𝘪𝘧𝘺 𝘢𝘯𝘥 𝘚𝘰𝘶𝘯𝘥𝘤𝘭𝘰𝘶𝘥 𝘢𝘳𝘦 𝘪𝘯 𝘣𝘦𝘵𝘢❓
+// ===========================================================================================================================
 module.exports.help = {
   name: "nightcore",
   aliases: ["nc"],
