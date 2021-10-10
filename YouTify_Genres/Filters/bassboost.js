4fed28ea326a12042f846f89abaa561302dc39ce
HypeVoidSoul <hypevoidsoul@gmail.com>
2021-10-10 06:53:24 +0100

    🍏YouTify™ v8 Huge Rework 🍏 + New Filters

--- YouTify_Genres/Filters/bassboost.js
+++ YouTify_Genres/Filters/bassboost.js
@@ -5,13 +5,64 @@ const { MessageEmbed } = require("discord.js");
 // ===========================================================================================================================
 module.exports.run = async (client, message, args, Discord) => {
   const Channel = message.member.voice.channel;
-  if (!Channel) return message.channel.send("Error: No Voice Channel!");
-
+  if (!Channel) {
+    message.react("🟡");
+    message.channel
+      .send(
+        new MessageEmbed()
+          .setTimestamp()
+          .setColor("#c4b932")
+          .setTitle(`\`💬Bassboost\``)
+          .setAuthor(
+            `🍏YouTify™`,
+            `https://i.postimg.cc/gcX8075z/guitar-sing.gif`
+          )
+          .setURL("https://github.com/Krakinz")
+          .setThumbnail(`https://i.postimg.cc/9f0mS5NY/YouTify.png`)
+          .setFooter(
+            `👈🏽‍Reqstd by ${message.author.username}`,
+            message.author.avatarURL({ dynamic: true })
+          )
+          .addField(
+            `\`☣️Error\``,
+            `**Not connected to any VoiceChannel!**`,
+            true
+          )
+      )
+      .catch(console.error);
+    return;
+  }
+  // ===========================================================================================================================
+  // 🍏𝐘𝐨𝐮𝐓𝐢𝐟𝐲™ is Discord 𝐘𝐎𝐔𝐓𝐔𝐁𝐄 Music Bot built with Discord.js and has 𝟐𝟎+ 𝐀𝐮𝐝𝐢𝐨 𝐅𝐢𝐥𝐭𝐞𝐫𝐬. ❓𝘚𝘱𝘰𝘵𝘪𝘧𝘺 𝘢𝘯𝘥 𝘚𝘰𝘶𝘯𝘥𝘤𝘭𝘰𝘶𝘥 𝘢𝘳𝘦 𝘪𝘯 𝘣𝘦𝘵𝘢❓
+  // ===========================================================================================================================
   const Queue = client.queue.get(message.guild.id);
-  if (!Queue)
-    return message.channel.send(
-      "Error: No Queue, Please Add Some Songs By Using Play & Search Command!"
-    );
+  if (!Queue) {
+    message.react("🟡");
+    message.channel
+      .send(
+        new MessageEmbed()
+          .setTimestamp()
+          .setColor("#c4b932")
+          .setTitle(`\`💬Bassboost\``)
+          .setAuthor(
+            `🍏YouTify™`,
+            `https://i.postimg.cc/gcX8075z/guitar-sing.gif`
+          )
+          .setURL("https://github.com/Krakinz")
+          .setThumbnail(`https://i.postimg.cc/9f0mS5NY/YouTify.png`)
+          .setFooter(
+            `👈🏽‍Reqstd by ${message.author.username}`,
+            message.author.avatarURL({ dynamic: true })
+          )
+          .addField(
+            `\`☣️Error\``,
+            `**No song in Queue, Please Add Some Songs By Using  ${client.YouTix}play or  ${client.YouTix}search Command!**`,
+            true
+          )
+      )
+      .catch(console.error);
+    return;
+  }
   // ===========================================================================================================================
   // 🍏𝐘𝐨𝐮𝐓𝐢𝐟𝐲™ is Discord 𝐘𝐎𝐔𝐓𝐔𝐁𝐄 Music Bot built with Discord.js and has 𝟐𝟎+ 𝐀𝐮𝐝𝐢𝐨 𝐅𝐢𝐥𝐭𝐞𝐫𝐬. ❓𝘚𝘱𝘰𝘵𝘪𝘧𝘺 𝘢𝘯𝘥 𝘚𝘰𝘶𝘯𝘥𝘤𝘭𝘰𝘶𝘥 𝘢𝘳𝘦 𝘪𝘯 𝘣𝘦𝘵𝘢❓
   // ===========================================================================================================================
@@ -20,19 +71,36 @@ module.exports.run = async (client, message, args, Discord) => {
     Filter: true,
     Song: Queue.Songs[0],
   });
-  message.channel.send(
-    new MessageEmbed()
-      .setColor(client.Color)
-      .setAuthor("Bassboost", message.author.avatarURL({ dynamic: true }))
-      .setDescription(
-        `Bassboost Filter Has Been ${
-          Queue.Filters["bassboost"]
-            ? "Disabled (Song Maybe Backward)"
-            : "Enabled (Song Maybe Ahead)"
-        }!`
-      )
-      .setTimestamp()
-  );
+  message.react("🟢");
+  message.channel
+    .send(
+      new MessageEmbed()
+        .setTimestamp()
+        .setColor("#43745a")
+        .setTitle(`\`💬Bassboost\``)
+        .setAuthor(
+          `🍏YouTify™`,
+          `https://i.postimg.cc/gcX8075z/guitar-sing.gif`
+        )
+        .setURL("https://github.com/Krakinz")
+        .setThumbnail(`https://i.postimg.cc/9f0mS5NY/YouTify.png`)
+        .setFooter(`👈🏽‍Reqstd by`, message.author.avatarURL({ dynamic: true }))
+        .addField(
+          `\`👄Filter Applied\``,
+          `**Bassboost Filter Has Been ${
+            Queue.Filters["bassboost"]
+              ? "Disabled (Song Maybe Backward)"
+              : "Enabled (Song Maybe Ahead)"
+          }!**`,
+          true
+        )
+        .addField(
+          `\`⚡Filter Warning!\``,
+          `**Note That Filter takes time to be applied to the current music as it depends on the size and lenth of audio!**`,
+          true
+        )
+    )
+    .catch(console.error);
   return;
 };
 // ===========================================================================================================================
@@ -43,7 +111,7 @@ module.exports.help = {
   aliases: ["bb"],
   cooldown: 7000,
   category: "Filters",
-  description: "Add/Remove Bassboost Filter To/From Currently Playing Song!",
-  usage: "Bassboost",
+  description: "Add/Remove bassboost Filter To/From Currently Playing Song!",
+  usage: "bassboost",
   examples: ["bassboost"],
 };
